4d803c8169a5b13a3f086ad313f11fb3cf1353bf
Antônio Marcos <antoniomarcos.s.rosa@gmail.com>
2023-02-02 08:57:32 -0300

    Second class: Complete

--- lib/app/pages/home/home_controller.dart
+++ lib/app/pages/home/home_controller.dart
@@ -1,6 +1,6 @@
+import 'dart:developer';
 import 'package:bloc/bloc.dart';
 import 'package:dw9_delivery_app/app/repositories/products_repository.dart';
-
 import 'home_state.dart';
 
 class HomeController extends Cubit<HomeState> {
@@ -13,6 +13,13 @@ class HomeController extends Cubit<HomeState> {
     try {
       final products = await _productsRepository.findAllProducts();
       emit(state.copyWith(status: HomeStateStatus.loaded, products: products));
-    } catch (e) {}
+    } catch (e, s) {
+      log('Erro ao buscar produtos.', error: e, stackTrace: s);
+      emit(
+        state.copyWith(
+            status: HomeStateStatus.error,
+            errorMessage: 'Erro ao buscar produtos.'),
+      );
+    }
   }
 }
